9d88aa64605252d6688bfb161e81cd4681c02914
faheem4531 <faheem4531@gmail.com>
2024-06-23 02:03:30 +0500

    add avtar component for images instead image tag

--- SpotGallery/react-frontend/src/user/components/UserItem.js
+++ SpotGallery/react-frontend/src/user/components/UserItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import './UserItem.css';
+import Avatar from '../../shared/components/UIElements/Avatar';
 
 const UserItem = props => {
   return (
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
