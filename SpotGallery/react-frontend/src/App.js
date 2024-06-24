5224de0dbf72b33e2c9fb85274e1b217b15601b4
faheem4531 <faheem4531@gmail.com>
2024-06-24 11:41:58 +0500

    Rendring user places & use dynamic routes

--- SpotGallery/react-frontend/src/App.js
+++ SpotGallery/react-frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
