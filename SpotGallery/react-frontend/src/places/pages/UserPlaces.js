3f6041191b1e8d983b7ada37115025acb6171c8b
faheem4531 <faheem4531@gmail.com>
2024-06-24 11:53:07 +0500

    showing the places of only that user ( by getting a route params)

--- SpotGallery/react-frontend/src/places/pages/UserPlaces.js
+++ SpotGallery/react-frontend/src/places/pages/UserPlaces.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useParams } from 'react-router-dom';
 import PlaceList from '../components/PlaceList';
 
 const DUMMY_PLACES = [
@@ -30,8 +31,10 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
 
-  return <PlaceList items={DUMMY_PLACES} />;
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
