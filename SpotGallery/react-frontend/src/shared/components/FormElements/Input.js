6634c966237cbfdc8b4bcadc0051173b1718789d
faheem4531 <faheem4531@gmail.com>
2024-07-03 02:39:33 +0500

    "New Place"  form validation

--- SpotGallery/react-frontend/src/shared/components/FormElements/Input.js
+++ SpotGallery/react-frontend/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -21,12 +21,20 @@ const inputReducer = (state, action) => {
       return state;
   }
 };
+
 const Input = props => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: '',
     isTouched: false,
     isValid: false
   });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput]);
 
   const changeHandler = event => {
     dispatch({
