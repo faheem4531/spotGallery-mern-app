1287ee7a243d706f6e4868b385f7a091477c05be
faheem4531 <faheem4531@gmail.com>
2024-07-05 02:07:00 +0500

    add auth page. (login btn)

--- SpotGallery/react-frontend/src/App.js
+++ SpotGallery/react-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
+import Auth from './user/pages/Auth';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
@@ -24,6 +25,9 @@ function App() {
           </Route>
           <Route path="/places/:placeId" >
             <UpdatePlace />
+          </Route>
+          <Route path="/auth" >
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
