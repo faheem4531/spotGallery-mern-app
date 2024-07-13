d24cbaf1b611fc4e0586ec660ad0d8bb98008218
faheem4531 <faheem4531@gmail.com>
2024-07-14 04:14:27 +0500

    login request

--- SpotGallery/node-backend/controllers/users-controllers.js
+++ SpotGallery/node-backend/controllers/users-controllers.js
@@ -36,7 +36,14 @@ const signup = (req, res, next) => {
 };
 
 const login = (req, res, next) => {
+  const { email, password } = req.body;
 
+  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+  }
+
+  res.json({ message: 'Logged in!' });
 };
 
 exports.getUsers = getUsers;
