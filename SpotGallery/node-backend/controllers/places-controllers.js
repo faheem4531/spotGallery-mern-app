775cd3f7708bbc73ebb4c2a65a2e29e55664d753
faheem4531 <faheem4531@gmail.com>
2024-07-14 03:44:55 +0500

    delete a place by id

--- SpotGallery/node-backend/controllers/places-controllers.js
+++ SpotGallery/node-backend/controllers/places-controllers.js
@@ -1,7 +1,7 @@
 const uuid = require('uuid/package.json');
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
@@ -81,6 +81,9 @@ const updatePlace = (req, res, next) => {
 
 
 const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+  res.status(200).json({ message: 'Deleted place.' });
 };
 
 exports.getPlaceById = getPlaceById;
