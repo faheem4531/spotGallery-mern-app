0133dc5b594a857bbeedd9beb1fd815e7c9b43fc
faheem4531 <faheem4531@gmail.com>
2024-07-14 02:54:13 +0500

    adding controler files for places routes

--- SpotGallery/node-backend/routes/places-routes.js
+++ SpotGallery/node-backend/routes/places-routes.js
@@ -1,44 +1,11 @@
 const express = require('express');
-const HttpError = require('../models/http-error')
+
+const placesControllers = require('../controllers/places-controllers');
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Islamabad',
-    description: 'my place',
-    location: {
-      lat: 40.3232,
-      lng: 32.123
-    },
-    address: "kasur",
-    creator: "u1"
-  }
-];
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid; //{pid: 'p1}
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  })
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.");
-  }
-  res.json({ place });
-});
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(p => {
-    return p.creator === userId;
-  })
-
-  if (!place) {
-    return next(new HttpError("Could not find a place for the provided user id."));
-  }
-  res.json({ place });
-});
-
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
 module.exports = router;
