6579705a1025bf1163418c4372262c5c3d4c4919
faheem4531 <faheem4531@gmail.com>
2024-07-14 02:41:01 +0500

    creating our own error model

--- SpotGallery/node-backend/routes/places-routes.js
+++ SpotGallery/node-backend/routes/places-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const HttpError = require('../models/http-error')
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -23,9 +23,7 @@ router.get('/:pid', (req, res, next) => {
   })
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided id.")
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find a place for the provided id.");
   }
   res.json({ place });
 });
@@ -37,9 +35,7 @@ router.get('/user/:uid', (req, res, next) => {
   })
 
   if (!place) {
-    const error = new Error("Could not find a place for the provided user id.")
-    error.code = 404;
-    return next(error);
+    return next(new HttpError("Could not find a place for the provided user id."));
   }
   res.json({ place });
 });
