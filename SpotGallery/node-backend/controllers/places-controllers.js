2e83665c20f28ac057db644793812c7c9225efe4
faheem4531 <faheem4531@gmail.com>
2024-07-14 03:49:23 +0500

    get places (not one place) of user id, fix minor issues

--- SpotGallery/node-backend/controllers/places-controllers.js
+++ SpotGallery/node-backend/controllers/places-controllers.js
@@ -32,14 +32,14 @@ const getPlaceById = (req, res, next) => {
 // function getPlaceById() { ... }
 // const getPlaceById = function() { ... }
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find(p => {
+  const places = DUMMY_PLACES.filter(p => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
       new HttpError('Could not find a place for the provided user id.', 404)
     );
@@ -87,7 +87,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
