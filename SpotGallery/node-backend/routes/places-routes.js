c63fafcbdaacefb93b59703f9a5b98c7592b1c13
faheem4531 <faheem4531@gmail.com>
2024-07-13 23:45:56 +0500

    add place-specific routes

--- SpotGallery/node-backend/routes/places-routes.js
+++ SpotGallery/node-backend/routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log("");
-  res.json({ message: "It Works!" });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Islamabad',
+    description: 'my place',
+    location: {
+      lat: 40.3232,
+      lng: 32.123
+    },
+    address: "kasur",
+    creator: "u1"
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid; //{pid: 'p1}
+  const place = DUMMY_PLACES.find(p => {
+    return p.id == placeId;
+  })
+  res.json({ place });
 });
 
 module.exports = router;
