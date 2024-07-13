b2c3f5bcd4e3bd1be857b8aae636a97180fb22b5
faheem4531 <faheem4531@gmail.com>
2024-07-14 03:41:25 +0500

    update places

--- SpotGallery/node-backend/controllers/places-controllers.js
+++ SpotGallery/node-backend/controllers/places-controllers.js
@@ -65,6 +65,26 @@ const createPlace = (req, res, next) => {
 };
 
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+
+const deletePlace = (req, res, next) => {
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- SpotGallery/node-backend/routes/places-routes.js
+++ SpotGallery/node-backend/routes/places-routes.js
@@ -10,4 +10,9 @@ router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
 router.post('/', placesControllers.createPlace);
 
+router.patch('/:pid', placesControllers.updatePlace);
+
+router.delete('/:pid', placesControllers.deletePlace);
+
+
 module.exports = router;
