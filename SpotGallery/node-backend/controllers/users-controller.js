a3417fa0be292e89ae059d907ce0021720aed73d
faheem4531 <faheem4531@gmail.com>
2024-07-14 04:00:39 +0500

    get users fromm the array

---
+++ SpotGallery/node-backend/controllers/users-controller.js
@@ -0,0 +1,28 @@
+const uuid = require('uuid/v4');
+
+const HttpError = require('../models/http-error');
+
+const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Max Schwarz',
+    email: 'test@test.com',
+    password: 'testers'
+  }
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+
+};
+
+const login = (req, res, next) => {
+
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
--- SpotGallery/node-backend/routes/users-routes.js
+++ SpotGallery/node-backend/routes/users-routes.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const express = require('express');
+
+const usersController = require('../controllers/users-controllers');
+
+const router = express.Router();
+
+router.get('/', usersController.getUsers);
+
+router.post('/signup', usersController.signup);
+
+router.post('/login', usersController.login);
+
+module.exports = router;
