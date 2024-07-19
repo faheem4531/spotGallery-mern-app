adce891dd1cc117283b69b91074f4fba0550b70e
faheem4531 <faheem4531@gmail.com>
2024-07-20 01:00:25 +0500

    update the places in the database with mongoose

--- SpotGallery/node-backend/controllers/places-controllers.js
+++ SpotGallery/node-backend/controllers/places-controllers.js
@@ -98,24 +98,38 @@ const createPlace = async (req, res, next) => {
 };
 
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     throw new HttpError('InValid inputs passed, please check your data.', 422)
   }
 
-
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError("Something went wrong, could not find place", 500);
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+    return next(error);
+  }
 
-  res.status(200).json({ place: updatedPlace });
+  place.title = title;
+  place.description = description;
+
+
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError("Something went wrong, could not update place", 500);
+
+    return next(error);
+  }
+
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 
