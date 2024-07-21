68c7ca157c78894aea087f4483831e986a95fbeb
faheem4531 <faheem4531@gmail.com>
2024-07-21 21:44:56 +0500

    adding the relation bw places and users

--- SpotGallery/node-backend/controllers/users-controllers.js
+++ SpotGallery/node-backend/controllers/users-controllers.js
@@ -26,7 +26,7 @@ const signup = async (req, res, next) => {
       new HttpError('Invalid inputs passed, please check your data.', 422)
     );
   }
-  const { name, email, password, places } = req.body;
+  const { name, email, password } = req.body;
 
   let existingUser
   try {
@@ -52,7 +52,7 @@ const signup = async (req, res, next) => {
     email,
     image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
     password,
-    places
+    places: []
   });
 
   try {
--- SpotGallery/node-backend/models/user.js
+++ SpotGallery/node-backend/models/user.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
-  places: { type: String, required: true },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }] //create the relation bw places and user, but user can have more than one place thats why we create the array []
 });
 
 userSchema.plugin(uniqueValidator)
